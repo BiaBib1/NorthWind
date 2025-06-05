94f3e79bb03e120561f5c4ae513cae804b4c0461
BiaBib1 <biabibi.urko@gmail.com>
2025-06-05 11:03:51 +0200

    up

--- northwind_dump/northwind_dump.sql
+++ northwind_dump/northwind_dump.sql
@@ -3596,13 +3596,6 @@ COPY public.orders (order_id, customer_id, employee_id, order_date, required_dat
 --
 
 COPY public.products (product_id, product_name, supplier_id, category_id, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued, caracteristicas_json) FROM stdin;
-1	Chai	8	1	10 boxes x 30 bags	18	39	0	10	1	{"categoria": "Beverages", "subcategoria": "Tea"}
-2	Chang	1	1	24 - 12 oz bottles	19	17	40	25	1	{"categoria": "Condiments", "subcategoria": "Sauces"}
-3	Aniseed Syrup	1	2	12 - 550 ml bottles	10	13	70	25	0	{"categoria": "Confections", "subcategoria": "Candy"}
-4	Chef Anton's Cajun Seasoning	2	2	48 - 6 oz jars	22	53	0	0	0	{"categoria": "Dairy Products", "subcategoria": "Cheese"}
-5	Chef Anton's Gumbo Mix	2	2	36 boxes	21.35	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": "Pasta"}
-6	Grandma's Boysenberry Spread	3	2	12 - 8 oz jars	25	120	0	25	0	{"categoria": "Meat/Poultry", "subcategoria": "Chicken"}
-21	Sir Rodney's Scones	8	3	24 pkgs. x 4 pieces	10	3	40	5	0	{"categoria": "Grains/Cereals", "subcategoria": "Rice"}
 22	Gustaf's Knäckebröd	9	5	24 - 500 g pkgs.	21	104	0	25	0	{"categoria": "Meat/Poultry", "subcategoria": "Pork"}
 23	Tunnbröd	9	5	12 - 250 g pkgs.	9	61	0	25	0	{"categoria": "Produce", "subcategoria": "Herbs"}
 24	Guaraná Fantástica	10	1	12 - 355 ml cans	4.5	20	0	0	1	{"categoria": "Seafood", "subcategoria": "Crustaceans"}
@@ -3610,6 +3603,49 @@ COPY public.products (product_id, product_name, supplier_id, category_id, quanti
 26	Gumbär Gummibärchen	11	3	100 - 250 g bags	31.23	15	0	0	0	{"categoria": "Condiments", "subcategoria": "Ketchup"}
 27	Schoggi Schokolade	11	3	100 - 100 g pieces	43.9	49	0	30	0	{"categoria": "Confections", "subcategoria": "Gummies"}
 28	Rössle Sauerkraut	12	7	25 - 825 g cans	45.6	26	0	0	1	{"categoria": "Dairy Products", "subcategoria": "Butter"}
+29	Thüringer Rostbratwurst	12	6	50 bags x 30 sausgs.	123.79	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": "Oats"}
+30	Nord-Ost Matjeshering	13	8	10 - 200 g glasses	25.89	10	0	15	0	{"categoria": "Meat/Poultry", "subcategoria": "Turkey"}
+31	Gorgonzola Telino	14	4	12 - 100 g pkgs	12.5	0	70	20	0	{"categoria": "Produce", "subcategoria": "Nuts"}
+32	Mascarpone Fabioli	14	4	24 - 200 g pkgs.	32	9	40	25	0	{"categoria": "Seafood", "subcategoria": "Tuna"}
+33	Geitost	15	4	500 g	2.5	112	0	20	0	{"categoria": "Beverages", "subcategoria": "Energy Drinks"}
+46	Spegesild	21	8	4 - 450 g glasses	12	95	0	0	0	{"categoria": "Meat/Poultry", "subcategoria": "Duck"}
+47	Zaanse koeken	22	3	10 - 4 oz boxes	9.5	36	0	0	0	{"categoria": "Produce", "subcategoria": "Onions"}
+48	Chocolade	22	3	10 pkgs.	12.75	15	70	25	0	{"categoria": "Seafood", "subcategoria": "Shrimp"}
+49	Maxilaku	23	3	24 - 50 g pkgs.	20	10	60	15	0	{"categoria": "Beverages", "subcategoria": "Sparkling Water"}
+50	Valkoinen suklaa	23	3	12 - 100 g bars	16.25	65	0	30	0	{"categoria": "Dessert", "subcategoria": "Chocolate"}
+51	Manjimup Dried Apples	24	7	50 - 300 g pkgs.	53	20	0	10	0	{"categoria": "Fruits", "subcategoria": "Apple"}
+52	Filo Mix	24	5	16 - 2 kg boxes	7	38	0	25	0	{"categoria": "Dairy Products", "subcategoria": null}
+53	Perth Pasties	24	6	48 pieces	32.8	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": null}
+54	Tourtière	25	6	16 pies	7.45	21	0	10	0	{"categoria": "Meat/Poultry", "subcategoria": null}
+55	Pâté chinois	25	6	24 boxes x 2 pies	24	115	0	20	0	{"categoria": "Meat", "subcategoria": "Poultry"}
+56	Gnocchi di nonna Alice	26	5	24 - 250 g pkgs.	38	21	10	30	0	{"categoria": "Gnocchi", "subcategoria": "Potatoes"}
+57	Ravioli Angelo	26	5	24 - 250 g pkgs.	19.5	36	0	20	0	{"categoria": "Pasta", "subcategoria": "Cereals"}
+58	Escargots de Bourgogne	27	8	24 pieces	13.25	62	0	20	0	{"categoria": "Condiments", "subcategoria": null}
+59	Raclette Courdavault	28	4	5 kg pkg.	55	79	0	0	0	{"categoria": "Cheese", "subcategoria": "Lactose"}
+60	Camembert Pierrot	28	4	15 - 300 g rounds	34	19	0	0	0	{"categoria": "Cheese", "subcategoria": "Lactose"}
+61	Sirop d'érable	29	2	24 - 500 ml bottles	28.5	113	0	25	0	{"categoria": "Syrup", "subcategoria": "Sugar"}
+62	Tarte au sucre	29	3	48 pies	49.3	17	0	0	0	{"categoria": "Cereales", "subcategoria": "Sugar"}
+63	Vegie-spread	7	2	15 - 625 g jars	43.9	24	0	5	0	{"categoria": "Produce", "subcategoria": null}
+64	Wimmers gute Semmelknödel	12	5	20 bags x 4 pieces	33.25	22	80	30	0	{"categoria": "Seafood", "subcategoria": null}
+65	Louisiana Fiery Hot Pepper Sauce	2	2	32 - 8 oz bottles	21.05	76	0	0	0	{"categoria": "Beverages", "subcategoria": "Spicy"}
+66	Louisiana Hot Spiced Okra	2	2	24 - 8 oz jars	17	4	100	20	0	{"categoria": "Condiments", "subcategoria": "Spicy"}
+67	Laughing Lumberjack Lager	16	1	24 - 12 oz bottles	14	52	0	10	0	{"categoria": "Confections", "subcategoria": null}
+68	Scottish Longbreads	8	3	10 boxes x 8 pieces	12.5	6	10	15	0	{"categoria": "Dairy Products", "subcategoria": "Cereals"}
+69	Gudbrandsdalsost	15	4	10 kg pkg.	36	26	0	15	0	{"categoria": "Grains/Cereals", "subcategoria": null}
+70	Outback Lager	7	1	24 - 355 ml bottles	15	15	10	30	0	{"categoria": "Meat/Poultry", "subcategoria": null}
+71	Flotemysost	15	4	10 - 500 g pkgs.	21.5	26	0	0	0	{"categoria": "Produce", "subcategoria": null}
+72	Mozzarella di Giovanni	14	4	24 - 200 g pkgs.	34.8	14	0	0	0	{"categoria": "Cheese", "subcategoria": "Mozzarella"}
+73	Röd Kaviar	17	8	24 - 150 g jars	15	101	0	5	0	{"categoria": "Beverages", "subcategoria": null}
+74	Longlife Tofu	4	7	5 kg pkg.	10	4	20	5	0	{"categoria": "Cereals", "subcategoria": "Soy"}
+75	Rhönbräu Klosterbier	12	1	24 - 0.5 l bottles	7.75	125	0	25	0	{"categoria": "Confections", "subcategoria": null}
+76	Lakkalikööri	23	1	500 ml	18	57	0	20	0	{"categoria": "Dairy Products", "subcategoria": null}
+77	Original Frankfurter grüne Soße	12	2	12 boxes	13	32	0	15	0	{"categoria": "Grains/Cereals", "subcategoria": null}
+1	Chai	8	1	10 boxes x 30 bags	18	39	0	10	1	{"categoria": "Beverages", "subcategoria": "Tea"}
+2	Chang	1	1	24 - 12 oz bottles	19	17	40	25	1	{"categoria": "Condiments", "subcategoria": "Sauces"}
+3	Aniseed Syrup	1	2	12 - 550 ml bottles	10	13	70	25	0	{"categoria": "Confections", "subcategoria": "Candy"}
+4	Chef Anton's Cajun Seasoning	2	2	48 - 6 oz jars	22	53	0	0	0	{"categoria": "Dairy Products", "subcategoria": "Cheese"}
+5	Chef Anton's Gumbo Mix	2	2	36 boxes	21.35	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": "Pasta"}
+6	Grandma's Boysenberry Spread	3	2	12 - 8 oz jars	25	120	0	25	0	{"categoria": "Meat/Poultry", "subcategoria": "Chicken"}
 7	Uncle Bob's Organic Dried Pears	3	7	12 - 1 lb pkgs.	30	15	0	10	0	{"categoria": "Produce", "subcategoria": "Vegetables"}
 8	Northwoods Cranberry Sauce	3	2	12 - 12 oz jars	40	6	0	0	0	{"categoria": "Seafood", "subcategoria": "Fish"}
 9	Mishi Kobe Niku	4	6	18 - 500 g pkgs.	97	29	0	0	1	{"categoria": "Beverages", "subcategoria": "Coffee"}
@@ -3624,11 +3660,7 @@ COPY public.products (product_id, product_name, supplier_id, category_id, quanti
 18	Carnarvon Tigers	7	8	16 kg pkg.	62.5	42	0	0	0	{"categoria": "Condiments", "subcategoria": "Dressings"}
 19	Teatime Chocolate Biscuits	8	3	10 boxes x 12 pieces	9.2	25	0	5	0	{"categoria": "Confections", "subcategoria": "Pastries"}
 20	Sir Rodney's Marmalade	8	3	30 gift boxes	81	40	0	0	0	{"categoria": "Dairy Products", "subcategoria": "Ice Cream"}
-29	Thüringer Rostbratwurst	12	6	50 bags x 30 sausgs.	123.79	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": "Oats"}
-30	Nord-Ost Matjeshering	13	8	10 - 200 g glasses	25.89	10	0	15	0	{"categoria": "Meat/Poultry", "subcategoria": "Turkey"}
-31	Gorgonzola Telino	14	4	12 - 100 g pkgs	12.5	0	70	20	0	{"categoria": "Produce", "subcategoria": "Nuts"}
-32	Mascarpone Fabioli	14	4	24 - 200 g pkgs.	32	9	40	25	0	{"categoria": "Seafood", "subcategoria": "Tuna"}
-33	Geitost	15	4	500 g	2.5	112	0	20	0	{"categoria": "Beverages", "subcategoria": "Energy Drinks"}
+21	Sir Rodney's Scones	8	3	24 pkgs. x 4 pieces	10	3	40	5	0	{"categoria": "Grains/Cereals", "subcategoria": "Rice"}
 34	Sasquatch Ale	16	1	24 - 12 oz bottles	14	111	0	15	0	{"categoria": "Condiments", "subcategoria": "Mustard"}
 35	Steeleye Stout	16	1	24 - 12 oz bottles	18	20	0	15	0	{"categoria": "Confections", "subcategoria": "Cookies"}
 36	Inlagd Sill	17	8	24 - 250 g  jars	19	112	0	20	0	{"categoria": "Dairy Products", "subcategoria": "Cream Cheese"}
@@ -3641,38 +3673,6 @@ COPY public.products (product_id, product_name, supplier_id, category_id, quanti
 43	Ipoh Coffee	20	1	16 - 500 g tins	46	17	10	25	0	{"categoria": "Confections", "subcategoria": "Marshmallows"}
 44	Gula Malacca	20	2	20 - 2 kg bags	19.45	27	0	15	0	{"categoria": "Dairy Products", "subcategoria": "Sour Cream"}
 45	Rogede sild	21	8	1k pkg.	9.5	5	70	15	0	{"categoria": "Grains/Cereals", "subcategoria": "Granola"}
-46	Spegesild	21	8	4 - 450 g glasses	12	95	0	0	0	{"categoria": "Meat/Poultry", "subcategoria": "Duck"}
-47	Zaanse koeken	22	3	10 - 4 oz boxes	9.5	36	0	0	0	{"categoria": "Produce", "subcategoria": "Onions"}
-48	Chocolade	22	3	10 pkgs.	12.75	15	70	25	0	{"categoria": "Seafood", "subcategoria": "Shrimp"}
-49	Maxilaku	23	3	24 - 50 g pkgs.	20	10	60	15	0	{"categoria": "Beverages", "subcategoria": "Sparkling Water"}
-50	Valkoinen suklaa	23	3	12 - 100 g bars	16.25	65	0	30	0	{"categoria": "Condiments", "subcategoria": null}
-51	Manjimup Dried Apples	24	7	50 - 300 g pkgs.	53	20	0	10	0	{"categoria": "Confections", "subcategoria": null}
-52	Filo Mix	24	5	16 - 2 kg boxes	7	38	0	25	0	{"categoria": "Dairy Products", "subcategoria": null}
-53	Perth Pasties	24	6	48 pieces	32.8	0	0	0	1	{"categoria": "Grains/Cereals", "subcategoria": null}
-54	Tourtière	25	6	16 pies	7.45	21	0	10	0	{"categoria": "Meat/Poultry", "subcategoria": null}
-55	Pâté chinois	25	6	24 boxes x 2 pies	24	115	0	20	0	{"categoria": "Produce", "subcategoria": null}
-56	Gnocchi di nonna Alice	26	5	24 - 250 g pkgs.	38	21	10	30	0	{"categoria": "Seafood", "subcategoria": null}
-57	Ravioli Angelo	26	5	24 - 250 g pkgs.	19.5	36	0	20	0	{"categoria": "Beverages", "subcategoria": null}
-58	Escargots de Bourgogne	27	8	24 pieces	13.25	62	0	20	0	{"categoria": "Condiments", "subcategoria": null}
-59	Raclette Courdavault	28	4	5 kg pkg.	55	79	0	0	0	{"categoria": "Confections", "subcategoria": null}
-60	Camembert Pierrot	28	4	15 - 300 g rounds	34	19	0	0	0	{"categoria": "Dairy Products", "subcategoria": null}
-61	Sirop d'érable	29	2	24 - 500 ml bottles	28.5	113	0	25	0	{"categoria": "Grains/Cereals", "subcategoria": null}
-62	Tarte au sucre	29	3	48 pies	49.3	17	0	0	0	{"categoria": "Meat/Poultry", "subcategoria": null}
-63	Vegie-spread	7	2	15 - 625 g jars	43.9	24	0	5	0	{"categoria": "Produce", "subcategoria": null}
-64	Wimmers gute Semmelknödel	12	5	20 bags x 4 pieces	33.25	22	80	30	0	{"categoria": "Seafood", "subcategoria": null}
-65	Louisiana Fiery Hot Pepper Sauce	2	2	32 - 8 oz bottles	21.05	76	0	0	0	{"categoria": "Beverages", "subcategoria": null}
-66	Louisiana Hot Spiced Okra	2	2	24 - 8 oz jars	17	4	100	20	0	{"categoria": "Condiments", "subcategoria": null}
-67	Laughing Lumberjack Lager	16	1	24 - 12 oz bottles	14	52	0	10	0	{"categoria": "Confections", "subcategoria": null}
-68	Scottish Longbreads	8	3	10 boxes x 8 pieces	12.5	6	10	15	0	{"categoria": "Dairy Products", "subcategoria": null}
-69	Gudbrandsdalsost	15	4	10 kg pkg.	36	26	0	15	0	{"categoria": "Grains/Cereals", "subcategoria": null}
-70	Outback Lager	7	1	24 - 355 ml bottles	15	15	10	30	0	{"categoria": "Meat/Poultry", "subcategoria": null}
-71	Flotemysost	15	4	10 - 500 g pkgs.	21.5	26	0	0	0	{"categoria": "Produce", "subcategoria": null}
-72	Mozzarella di Giovanni	14	4	24 - 200 g pkgs.	34.8	14	0	0	0	{"categoria": "Seafood", "subcategoria": null}
-73	Röd Kaviar	17	8	24 - 150 g jars	15	101	0	5	0	{"categoria": "Beverages", "subcategoria": null}
-74	Longlife Tofu	4	7	5 kg pkg.	10	4	20	5	0	{"categoria": "Condiments", "subcategoria": null}
-75	Rhönbräu Klosterbier	12	1	24 - 0.5 l bottles	7.75	125	0	25	0	{"categoria": "Confections", "subcategoria": null}
-76	Lakkalikööri	23	1	500 ml	18	57	0	20	0	{"categoria": "Dairy Products", "subcategoria": null}
-77	Original Frankfurter grüne Soße	12	2	12 boxes	13	32	0	15	0	{"categoria": "Grains/Cereals", "subcategoria": null}
 \.
 
 
